7df085f1f9995853adc284e357d4a23ba3e6205d
Bush Philip <fp.bush@gmail.com>
2022-08-13 18:53:44 +0300

    fix: fixing arrow rendering in orderBy

--- src/app/components/tableHeader.jsx
+++ src/app/components/tableHeader.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-
 const TableHeader = ({ onSort, selectedSort, columns }) => {
     const handleSort = (item) => {
         if (selectedSort.path === item) {
@@ -12,13 +11,15 @@ const TableHeader = ({ onSort, selectedSort, columns }) => {
             onSort({ path: item, order: "asc" });
         }
     };
-
-    const toggleCaret = (path) => {
-        const classes = "bi ms-1 bi-caret-";
-
-        if (selectedSort.path === path) {
-            return <i className={classes + (selectedSort.order === "asc" ? "up-fill" : "down-fill")} />;
+    const renderSortArrow = (selectedSort, currentPath) => {
+        if (selectedSort.path === currentPath) {
+            if (selectedSort.order === "asc") {
+                return <i className="bi bi-caret-down-fill"></i>;
+            } else {
+                return <i className="bi bi-caret-up-fill"></i>;
+            }
         }
+        return null;
     };
 
     return (
@@ -35,19 +36,18 @@ const TableHeader = ({ onSort, selectedSort, columns }) => {
                         {...{ role: columns[column].path && "button" }}
                         scope="col"
                     >
-                        {columns[column].name}
-                        {toggleCaret(columns[column].path)}
+                        {columns[column].name}{" "}
+                        {renderSortArrow(selectedSort, columns[column].path)}
                     </th>
                 ))}
             </tr>
         </thead>
     );
 };
-
 TableHeader.propTypes = {
-    onSort: PropTypes.func.isRequred,
-    selectedSort: PropTypes.object.isRequred,
-    columns: PropTypes.object.isRequred
+    onSort: PropTypes.func.isRequired,
+    selectedSort: PropTypes.object.isRequired,
+    columns: PropTypes.object.isRequired
 };
 
 export default TableHeader;
