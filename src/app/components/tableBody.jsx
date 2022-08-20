462ac62451567b96acc21e15aaf0112e9a0c71a9
Bush Philip <fp.bush@gmail.com>
2022-08-20 16:51:26 +0300

    feat: add routing

--- src/app/components/tableBody.jsx
+++ src/app/components/tableBody.jsx
@@ -6,29 +6,36 @@ const TableBody = ({ data, columns }) => {
     const renderContent = (item, column) => {
         if (columns[column].component) {
             const component = columns[column].component;
-            if (typeof component === "function") {
-                return component(item);
+
+            if (typeof component === "string") {
+                return component;
             }
-            return component;
+            return component(item);
+        } else {
+            return _.get(item, columns[column].path);
         }
-        return _.get(item, columns[column].path);
     };
+
     return (
         <tbody>
-            {data.map((item) => (
-                <tr key={item._id}>
-                    {Object.keys(columns).map((column) => (
-                        <td key={column}>{renderContent(item, column)}</td>
-                    ))}
-                </tr>
-            ))}
+            {
+                data.map(item => (
+                    <tr key={item._id}>
+                        {Object.keys(columns).map(column => (
+                            <td key={column}>
+                                {renderContent(item, column)}
+                            </td>
+                        ))}
+                    </tr>
+                ))
+            }
         </tbody>
     );
 };
 
 TableBody.propTypes = {
-    data: PropTypes.array.isRequred,
-    columns: PropTypes.object.isRequred
+    data: PropTypes.array.isRequired,
+    columns: PropTypes.object.isRequired
 };
 
 export default TableBody;
